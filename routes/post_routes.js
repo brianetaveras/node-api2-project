12d94487f3ae170d953df61017c09ad655b64e81
unknown <@Cungara17>
2020-02-18 08:06:18 -0500

    Made some endpoint fixes

--- routes/post_routes.js
+++ routes/post_routes.js
@@ -3,71 +3,87 @@ const router = express.Router();
 const db = require('../data/db');
 
 
-router.post('/', async (req, res)=>{
-    if (!req.body.title || !req.body.contents){
-        return res.status(400).json({
-            message: "Please make sure to include the title and content!"
+router.post('/', async (req, res) => {
+    try {
+        if (!req.body.title || !req.body.contents) {
+            return res.status(400).json({
+                message: "Please make sure to include the title and content!"
+            })
+        }
+        db.insert(req.body);
+        res.json({
+            ...req.body,
+            id: data.id
+        })
+
+    } catch {
+        res.status(500).json({
+            message: "Oops! There was an internal server error"
         })
     }
-    const data = await db.insert(req.body);
-    res.json({
-        ...req.body,
-        id: data.id
-    })
 })
 
-router.post('/:id/comments', async (req, res)=>{
-    try{
-        
+router.post('/:id/comments', async (req, res) => {
+    try {
+        !req.body.text ? res.status(400).json({message: "The text field is required"}) : null
         const post = await db.findById(req.params.id);
-    
-    
-        if (post){
+        if (post) {
             res.status(201).json(await db.insertComment(req.body))
         }
     } catch {
-        res.status(500).json('There was an issue adding the comment. Try again later.')
+        res.status(500).json('Please provide text for the comment')
     }
-    
+
 })
 
-router.get('/', async (req, res)=>{
-    try{
+router.get('/', async (req, res) => {
+    try {
         const posts = await db.find()
         res.json(posts)
-    } catch (err){
-        res.status(500).json({message: 'There was an error while getting the post list. Contact the Administrator.'})
+    } catch (err) {
+        res.status(500).json({ message: 'There was an error while getting the post list. Contact the Administrator.' })
     }
 
 }
 )
-router.get('/:id', async (req, res)=>{
-    try{
-        res.status(201).json(await db.findById(req.params.id))
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await db.findById(req.params.id);
+        if (post) {
+            return res.status(201).json(post);
+        }
+        res.status(404).json({ message: "The user was not found." })
+
     } catch {
         res.status(500).json({
             message: "Error while fetching the resource. Try again later!"
         })
     }
-    
+
 })
 
-router.get('/:id/comments', async (req, res)=>{
-    try{
-        res.json(await db.findPostComments(req.params.id))
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const post = await db.findById(req.params.id);
+        if (post){
+            return res.json(await db.findPostComments(req.params.id))
+        }
+        res.status(404).json({
+            message: "The specified post was not found."
+        })
     } catch {
-        res.json({message: "There was an error while fetching the comments. Try again later."})
+        res.json({ message: "There was an error while fetching the comments. Try again later." })
     }
-    
+
 })
 
 
-router.delete('/:id', (req, res)=>{
+router.delete('/:id', (req, res) => {
     res.json('yoyoyo, soy un post!')
-    
+
 })
 
-router.put('/:id', (req, res)=>{
+router.put('/:id', (req, res) => {
     res.json('yoyoyo, soy un post!')
 
 })
