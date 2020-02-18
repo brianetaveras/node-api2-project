b12522470ce30cfe7bf4874fe4f92770b9702e2b
brianetaveras <taveras574@gmail.com>
2020-02-17 21:22:26 -0500

    Get posts enpoints now works

--- routes/post_routes.js
+++ routes/post_routes.js
@@ -4,19 +4,16 @@ const db = require('../data/db');
 
 
 router.post('/', async (req, res)=>{
-
     if (!req.body.title || !req.body.contents){
         return res.status(400).json({
             message: "Please make sure to include the title and content!"
         })
     }
     const data = await db.insert(req.body);
-
     res.json({
         ...req.body,
         id: data.id
     })
-    
 })
 
 router.post('/:id/comments', (req, res)=>{
@@ -24,9 +21,14 @@ router.post('/:id/comments', (req, res)=>{
     
 })
 
-router.get('/', (req, res)=>{
-    res.json('yoyoyo, soy un post!')
-    
+router.get('/', async (req, res)=>{
+    try{
+        const posts = await db.find()
+        res.json(posts)
+    } catch (err){
+        res.status(500).json({message: 'There was an error while getting the post list. Contact the Administrator.'})
+    }
+
 }
 )
 router.get('/:id', (req, res)=>{
